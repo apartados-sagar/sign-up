db3fe06d2269a7bf7fe24598266b60d3410bed48
sagar-servicos <0.apartados.sagar.0@gmail.com>
2026-01-12 20:11:36 -0600

    cambio

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
+//const CONFIG = {
+  //  API_URL: 'http://localhost:10000',
+  //  API_KEY: 'fbx_WBePsq9IOwEkci72fwhIpvyvqnsDM1'  // MISMA que en Render
+//};
 const CONFIG = {
-    API_URL: 'https://back-end-0auy.onrender.com',
-    API_KEY: 'fbx_WBePsq9IOwEkci72fwhIpvyvqnsDM1'  // MISMA que en Render
+    API_URL: window.location.hostname === 'localhost' 
+        ? 'http://localhost:10000'  // Local
+        : 'https://back-end-0auy.onrender.com',  // Producción
+    API_KEY: 'fbx_WBePsq9IOwEkci72fwhIpvyvqnsDM1'
 };
